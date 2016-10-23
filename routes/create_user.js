1a816186ef77a8c4e7d74698452995fda856a144
Elliot Campbelton <e.b.campbelton@gmail.com>
2016-10-24 00:00:36 +0100

    d

--- routes/create_user.js
+++ routes/create_user.js
@@ -9,6 +9,13 @@ exports.createUser = function (req, res) {
 
     console.log("HI");
 
+    function randomString(length, chars) {
+            var result = '';
+            for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+            return result;
+    }
+    
+
     var mysql = require('mysql');
     var connection = mysql.createConnection({ host: 'localhost', user: 'root', password: 'smashing', database: 'MACRO' });
     connection.connect(function(err) { if (err) { console.error('error connecting: ' + err.stack); return; }});
