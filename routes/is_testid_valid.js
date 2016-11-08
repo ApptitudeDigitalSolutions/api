6e1c98029002c2240f927578794a79f461e33690
Elliot Campbelton <e.b.campbelton@gmail.com>
2016-11-08 16:16:52 +0000

    d

--- routes/is_testid_valid.js
+++ routes/is_testid_valid.js
@@ -15,7 +15,7 @@ exports.validateTestID = function (req, res) {
             // get count of sections
             var query = 'SELECT * FROM Test_templates WHERE id = '+testID+' AND DATE(to_be_conducted_on) = CURDATE();';
             connectionTo_TEST_MACRO.query(query, function(err, rows) {if (err) { console.log('Error SQL :' + err); return;} else {
-       console.log(query);
+       
             var testInfoJSON;
             for(i in rows){
 
@@ -28,8 +28,6 @@ exports.validateTestID = function (req, res) {
                                             test_title: test_title,
                                             description: description,
                                             test_ids_in_series: test_ids_in_series};
-
-                                            console.log(JSON.stringify(testInfoJSON));
      
             }
 
