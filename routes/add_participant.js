8ceeff23f4d1f22832d3d72e7e8531d9ec1128ff
Elliot Campbelton <e.b.campbelton@gmail.com>
2016-11-11 12:59:38 +0000

    r

--- routes/add_participant.js
+++ routes/add_participant.js
@@ -10,7 +10,7 @@ exports.addParticipant = function (req, res) {
     var DoB = req.body.dob;
     var other = req.body.other;
   
-    // var mysql = require('mysql');
+    var mysql = require('mysql');
     // var connection = mysql.createConnection({ host: 'localhost', user: 'root', password: 'smashing', database: 'MACRO' });
     // connection.connect(function(err) { if (err) { console.error('error connecting: ' + err.stack); return; }});
 
