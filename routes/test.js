89d84950bc0f2605881178261b8263d8d2a26f4e
Elliot Campbelton <e.b.campbelton@gmail.com>
2016-11-16 15:26:50 +0000

    d

--- routes/test.js
+++ routes/test.js
@@ -1,10 +1,10 @@
 exports.hi = function (req, res) {
-							res.writeHead(200, {
-                                "Content-Type": "application/json"
-                            });
-                            var json = JSON.stringify({
-                                passcode: passcode,
-                                company_id:companyID
-                            });
-                            res.end(json);
+							// res.writeHead(200, {
+       //                          "Content-Type": "application/json"
+       //                      });
+       //                      var json = JSON.stringify({
+       //                          passcode: passcode,
+       //                          company_id:companyID
+       //                      });
+       //                      res.end(json);
                         }
