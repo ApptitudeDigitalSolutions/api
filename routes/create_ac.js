11865e4dedeedcc9c534e8190a1793abec4745e6
Elliot Campbelton <e.b.campbelton@gmail.com>
2016-11-19 19:44:38 +0000

    d

--- routes/create_ac.js
+++ routes/create_ac.js
@@ -17,7 +17,7 @@ exports.createAC = function (req, res) {
     var activity_titles_array = activity_titles.split(",");
     var activity_types_array = activity_types.split(",");
 
-    var intertedrow ;
+    var intertedrows =[];
     var ac_id_generated;
 
     var arrayofActivity_ids = [];
@@ -52,46 +52,49 @@ exports.createAC = function (req, res) {
                 //console.log(query3);
                 connection.query(query3, function(err, result) {if (err) { console.log('Error : '+err); return;} else { 
                 console.log("STATUS >>>>> inserted new activity of type >> " + activity + " With title " + activity_title);
-                intertedrow = result.insertId;
+                intertedrows.push(result.insertId);
                 arrayofActivity_ids.push(intertedrow);
                     // now we need to creat the needed tables for each 
 
-                    if(activity == "i"){
-                        var arrayOfQuesries =["CREATE TABLE Interview_questions_"+intertedrow+" (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  question_id int(11),  section_id int(11) DEFAULT NULL,  section_title text,  section_text text,  section_media_url text,  section_media_type text,  question text,  prompts text,  answer_type int(11) DEFAULT NULL,  answer_options text,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Interview_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  section_id int(11) DEFAULT NULL,  answer_text text,  answer_wav text,  answer_notes text,   PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Interview_review_questions_"+intertedrow+" (  review_question_id int(11) unsigned NOT NULL AUTO_INCREMENT,  review_question text,  positive_indicators text,  negative_indicators text,  PRIMARY KEY (review_question_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Interview_review_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  answer_text text,  answer_selection text,  PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"]; 
-                        for(j in arrayOfQuesries){
-                            var thisquery = arrayOfQuesries[j];
-                            //console.log(thisquery);
-                            connection.query(thisquery, function(err, rows) {if (err) { console.log('Error : The SQL statement is realy batty'); return;} else { 
-                            
-                            }});
+                    for(k in intertedrow){
+                    var intertedrow = intertedrows[k];
+                        if(activity == "i"){
+                            var arrayOfQuesries =["CREATE TABLE Interview_questions_"+intertedrow+" (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  question_id int(11),  section_id int(11) DEFAULT NULL,  section_title text,  section_text text,  section_media_url text,  section_media_type text,  question text,  prompts text,  answer_type int(11) DEFAULT NULL,  answer_options text,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Interview_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  section_id int(11) DEFAULT NULL,  answer_text text,  answer_wav text,  answer_notes text,   PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Interview_review_questions_"+intertedrow+" (  review_question_id int(11) unsigned NOT NULL AUTO_INCREMENT,  review_question text,  positive_indicators text,  negative_indicators text,  PRIMARY KEY (review_question_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Interview_review_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  answer_text text,  answer_selection text,  PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"]; 
+                            for(j in arrayOfQuesries){
+                                var thisquery = arrayOfQuesries[j];
+                                //console.log(thisquery);
+                                connection.query(thisquery, function(err, rows) {if (err) { console.log('Error : The SQL statement is realy batty'); return;} else { 
+                                
+                                }});
+                            }
+                            console.log("STATUS >>>>> Created all INTERVIEW TABLES");
+                           
                         }
-                        console.log("STATUS >>>>> Created all INTERVIEW TABLES");
-                       
-                    }
 
-                    if(activity == "p"){
-                        var arrayOfQuesries =["CREATE TABLE Presentation_"+intertedrow+" (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  presentation_info_ref text,  presentation_info_text text,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Presentation_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  section_id int(11) DEFAULT NULL,  answer_text text,  answer_wav text,  answer_notes text,   PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Presentation_review_questions_"+intertedrow+" (  review_question_id int(11) unsigned NOT NULL AUTO_INCREMENT,  review_question text,  positive_indicators text,  negative_indicators text,  PRIMARY KEY (review_question_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Presentation_review_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  answer_text text,  answer_selection text,  PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"];
-                        for(j in arrayOfQuesries){
-                            var thisquery = arrayOfQuesries[j];
-                            //console.log(thisquery);
-                            connection.query(thisquery, function(err, rows) {if (err) { console.log('Error : The SQL statement is realy batty'); return;} else { 
-                            //console.log("Company successfully created");
-                            }});
+                        if(activity == "p"){
+                            var arrayOfQuesries =["CREATE TABLE Presentation_"+intertedrow+" (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  presentation_info_ref text,  presentation_info_text text,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Presentation_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  section_id int(11) DEFAULT NULL,  answer_text text,  answer_wav text,  answer_notes text,   PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Presentation_review_questions_"+intertedrow+" (  review_question_id int(11) unsigned NOT NULL AUTO_INCREMENT,  review_question text,  positive_indicators text,  negative_indicators text,  PRIMARY KEY (review_question_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Presentation_review_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  answer_text text,  answer_selection text,  PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"];
+                            for(j in arrayOfQuesries){
+                                var thisquery = arrayOfQuesries[j];
+                                //console.log(thisquery);
+                                connection.query(thisquery, function(err, rows) {if (err) { console.log('Error : The SQL statement is realy batty'); return;} else { 
+                                //console.log("Company successfully created");
+                                }});
+                            }
+                             console.log("STATUS >>>>> Created all PRESENTATION TABLES");
+
                         }
-                         console.log("STATUS >>>>> Created all PRESENTATION TABLES");
 
-                    }
-
-                    if(activity == "rp"){
-                        var arrayOfQuesries =["CREATE TABLE Roleplay_"+intertedrow+" (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  roleplay_info_ref text,  roleplay_info_text text,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Roleplay_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  section_id int(11) DEFAULT NULL,  answer_text text,  answer_wav text,  answer_notes text,   PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Roleplay_review_questions_"+intertedrow+" (  review_question_id int(11) unsigned NOT NULL AUTO_INCREMENT,  review_question text,  positive_indicators text,  negative_indicators text,  PRIMARY KEY (review_question_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Roleplay_review_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  answer_text text,  answer_selection text,  PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"];
-                        for(j in arrayOfQuesries){
-                            var thisquery = arrayOfQuesries[j];
-                            //console.log(thisquery);
-                            connection.query(thisquery, function(err, rows) {if (err) { console.log('Error : The SQL statement is realy batty'); return;} else { 
-                            //console.log("Company successfully created");
-                            }});
+                        if(activity == "rp"){
+                            var arrayOfQuesries =["CREATE TABLE Roleplay_"+intertedrow+" (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  roleplay_info_ref text,  roleplay_info_text text,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Roleplay_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  section_id int(11) DEFAULT NULL,  answer_text text,  answer_wav text,  answer_notes text,   PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Roleplay_review_questions_"+intertedrow+" (  review_question_id int(11) unsigned NOT NULL AUTO_INCREMENT,  review_question text,  positive_indicators text,  negative_indicators text,  PRIMARY KEY (review_question_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;","CREATE TABLE Roleplay_review_results_"+intertedrow+" (  result_id int(11) unsigned NOT NULL AUTO_INCREMENT,  candidate_id int(11) DEFAULT NULL,  question_id int(11) DEFAULT NULL,  answer_text text,  answer_selection text,  PRIMARY KEY (result_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"];
+                            for(j in arrayOfQuesries){
+                                var thisquery = arrayOfQuesries[j];
+                                //console.log(thisquery);
+                                connection.query(thisquery, function(err, rows) {if (err) { console.log('Error : The SQL statement is realy batty'); return;} else { 
+                                //console.log("Company successfully created");
+                                }});
+                            }
+                             console.log("STATUS >>>>> Created all ROLEPLAY TABLES");
                         }
-                         console.log("STATUS >>>>> Created all ROLEPLAY TABLES");
                     }
 
                    var queryq = 'UPDATE Assessment_Center_templates SET activity_ids = \'' + arrayofActivity_ids + '\' WHERE id = '+ac_id_generated+';';
