70833934c5d9cbd9466b7dade7175e68591ba93d
Elliot Campbelton <e.b.campbelton@gmail.com>
2017-03-13 18:49:15 +0000

    d

--- app.js
+++ app.js
@@ -159,16 +159,16 @@ function getNodeConfig(availibilityZone){
 function startServer(){
 	
 	
-	if (cluster.isMaster) {
-	  // Fork workers.
-	for (var i = 0; i < numCPUs; i++) {
-	cluster.fork();
-	}
+	// if (cluster.isMaster) {
+	//   // Fork workers.
+	// for (var i = 0; i < numCPUs; i++) {
+	// cluster.fork();
+	// }
 
-	cluster.on('exit', function(worker, code, signal) {
-	    console.log('worker ' + worker.process.pid + ' died');
-	 });
-	} else {
+	// cluster.on('exit', function(worker, code, signal) {
+	//     console.log('worker ' + worker.process.pid + ' died');
+	//  });
+	// } else {
 	https.createServer(options, app).listen(443); 
 		app.use(express.static(path.join(__dirname, '/public'))); 
 		app.use(bodyParser.urlencoded({
@@ -179,6 +179,6 @@ function startServer(){
 
 
 		var routes = require("./routes/routes.js")(app);
-	}
+	// }
 
 }
