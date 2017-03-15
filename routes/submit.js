77bcf9f3beb9f5f2e811ab4c1b005f4eb4d3c59f
Elliot Campbelton <e.b.campbelton@gmail.com>
2017-03-15 21:20:54 +0000

    d

--- routes/submit.js
+++ routes/submit.js
@@ -189,6 +189,7 @@ function getAC(callback){
             for(m in activity_results_for_candidate){
               var stringToInserIntoCell = activity_results_for_candidate[m].question_id+"|"+activity_results_for_candidate[m].answer_text+"|"+activity_results_for_candidate[m].answer_type;
               console.log(stringToInserIntoCell);
+              console.log("table >  " + activity_report.activity_report_components[j].table);
               activity_report.activity_report_components[j].table.push({cell:stringToInserIntoCell});
             }
 
