d5837a3a037cd436b22012109dfd4dd03190c9ac
Elliot Campbelton <e.b.campbelton@gmail.com>
2017-04-14 23:43:33 +0100

    d

--- routes/set_test_page.js
+++ routes/set_test_page.js
@@ -279,7 +279,9 @@ var authenticate = require("./auth.js");
           console.log('file saved');
 
           var fc = csv.toString('base64');
+          console.log(fc);
 
+          console.log(csv.toString());
           client.sendEmail({
                 "From": "elliotcampbelton@apptitudedigitalsolutions.com", 
                 "To": "e.b.campbelton@gmail.com", 
