9f1debc1cb3da4605b5d509822e1e7d8179ebd89
Elliot Campbelton <e.b.campbelton@gmail.com>
2017-04-20 22:59:33 +0100

    d

---
+++ routes/delete_Review_Resuklts_For_Candidate.js
@@ -0,0 +1,63 @@
+exports.nuke = function (req, res) {
+    var section_type = req.body.section_type;
+    var candidate_id = req.body.candidate_id;
+    
+    var username = req.body.username;
+    var passcode = req.body.passcode;
+    var ac_id = req.params.ac_id; 
+  
+    var mysql = require('mysql');
+     var connectionAC_MACRO = mysql.createConnection({ host: req.app.locals.AC_MACRO_DB_HOST, user: req.app.locals.AC_MACRO_DB_USER, password: req.app.locals.AC_MACRO_DB_PASSWORD, database: req.app.locals.AC_MACRO_DB_NAME });
+    connectionAC_MACRO.connect(function(err) { if (err) { console.error('error connecting: ' + err.stack); return; }});
+
+
+var authenticate = require("./auth.js");
+     authenticate.authenticate(username,passcode,req,function(returnValue) {
+      if(returnValue){
+          var async = require('async');
+          async.waterfall([deleteRecords], function (err, result) { console.log("DONE");  connectionAC_MACRO.end(); });
+      }else{
+          connectionAC_MACRO.end(); 
+          if(!res.headersSent){
+          res.writeHead(200, {
+              "Content-Type": "application/json"
+          });
+          var json = JSON.stringify({
+             status:"unauthorized"
+              });
+          res.end(json);
+        }
+      }
+  });
+
+
+    function deleteRecords(callback){
+    		// get count of sections
+        var query = "";
+
+        if(section_type == "i"){
+            query = 'DELETE FROM Interview_review_results_'+ac_id+' WHERE candidate_id = \''+candidate_id+'\';';
+        }
+
+        if(section_type == "rp"){
+            query = 'DELETE FROM Roleplay_review_results_'+ac_id+' WHERE candidate_id = \''+candidate_id+'\';';
+        }
+
+        if(section_type == "p"){
+          query = 'DELETE FROM Presentation_review_results_'+ac_id+' WHERE candidate_id = \''+candidate_id+'\';';
+        }
+
+        console.log(query);
+        connectionAC_MACRO.query(query, function(err, rows) {if (err) { console.log('Error SQL :' + err); return;} else {
+
+                 
+                  res.writeHead(200, {
+                      "Content-Type": "application/json"
+                  })
+                  var json = JSON.stringify({success:1});
+                  console.log('TEST STATE IS ........................... ' + json);
+                  res.end(json);
+                  connectionAC_MACRO.end();
+        	}});
+    }
+}
