c7de8b7eb7d9df7592175edf9d090229fa7474be
Elliot Campbelton <e.b.campbelton@gmail.com>
2017-07-07 15:57:21 +0100

    d

--- routes/submit.js
+++ routes/submit.js
@@ -204,11 +204,11 @@ connectionAC_MACRO.connect(function(err) { if (err) { console.error('error conne
               //activity_results_for_candidate = rows;
 
               // console.log(info);
-               // console.log("QUERY 1 " + results[0]); 
+                // console.log("QUERY 1 " + results[0]); 
                // console.log("QUERY 2 " + results[1]); 
                // console.log("QUERY 3 " + results[2]); 
 
-
+                console.log("QUERY 1 ");
 
               // we should get all results for a candidate in here
 
